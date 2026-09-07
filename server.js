f42ce8fd1494686864d13550b5eba51a7a817fd6
enricorebelo606-spec <enricorebelo606@gmail.com>
2026-09-07 22:51:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 // Rota raiz
 app.get('/', (req, res) => {
